5c594de32c1e1173231337a1acb0e6b486ba5a7c
Akshat <akshatwagadre007@gmail.com>
2020-09-10 02:18:55 +0530

    Initializing

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 
 const indexRouter = require("./routes/index");
-if (process.env.NODE_ENV !== "prodcution") {
-  require("dotenv").config();
-}
-
+// if (process.env.NODE_ENV !== "prodcution") {
+//   require("dotenv").config();
+// }
 // require("dotenv").config({ path: "./.env" });
+
+require("dotenv").config({ silent: process.env.NODE_ENV === "production" });
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
