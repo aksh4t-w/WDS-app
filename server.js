a7611a46c85ace236e760b075c8c528afafc6dd4
Akshat <akshatwagadre007@gmail.com>
2020-09-25 01:39:19 +0530

    Finished Author Routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 
 const indexRouter = require("./routes/index");
@@ -10,12 +11,12 @@ const bookRouter = require("./routes/books");
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-// require("dotenv").config({ path: "./.env" });
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
+app.use(methodOverride("_method"));
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ limit: "10mb", extended: false }));
